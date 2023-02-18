e1ff2617a6575ec5cafc81518da2f2aba8b835f9
junioranheu <junioranheu@gmail.com>
2023-02-18 00:48:01 -0300

    corrigir AutenticarController

--- src/Wards.API/Controllers/AutenticarController.cs
+++ src/Wards.API/Controllers/AutenticarController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Wards.Application.UsesCases.Usuarios.Shared.Models;
+using Wards.Application.UsesCases.Auths.Logar;
+using Wards.Domain.DTOs;
 using Wards.Domain.Entities;
 
 namespace Wards.API.Controllers
@@ -8,32 +9,32 @@ namespace Wards.API.Controllers
     [Route("api/[controller]")]
     public class AutenticarController : Controller
     {
-        private readonly IAutenticarService _autenticarService;
+        private readonly ILogarUseCase _logarService;
 
-        public AutenticarController(IAutenticarService autenticarService)
+        public AutenticarController(ILogarUseCase logarService)
         {
-            _autenticarService = autenticarService;
+            _logarService = logarService;
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UsuarioDTO>> Login(Usuario input)
+        public async Task<ActionResult<UsuarioDTO>> Logar(Usuario input)
         {
-            var authResultado = await _autenticarService.Login(input);
+            var authResultado = await _logarService.Logar(input);
             return Ok(authResultado);
         }
 
-        [HttpPost("registrar")]
-        public async Task<ActionResult<UsuarioDTO>> Registrar(Usuario input)
-        {
-            var authResultado = await _autenticarService.Registrar(input);
-            return Ok(authResultado);
-        }
+        //[HttpPost("registrar")]
+        //public async Task<ActionResult<Usuario>> Registrar(Usuario input)
+        //{
+        //    var authResultado = await _autenticarService.Registrar(input);
+        //    return Ok(authResultado);
+        //}
 
-        [HttpPost("refreshToken")]
-        public async Task<ActionResult<UsuarioDTO>> RefreshToken(UsuarioDTO input)
-        {
-            var authResultado = await _autenticarService.RefreshToken(input.Token, input.RefreshToken);
-            return Ok(authResultado);
-        }
+        //[HttpPost("refreshToken")]
+        //public async Task<ActionResult<Usuario>> RefreshToken(UsuarioDTO input)
+        //{
+        //    var authResultado = await _autenticarService.RefreshToken(input.Token, input.RefreshToken);
+        //    return Ok(authResultado);
+        //}
     }
 }
