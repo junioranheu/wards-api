ed4cfaeaac228e883b5fd5849232e0445028af2d
junioranheu <junioranheu@gmail.com>
2023-03-18 11:18:59 -0300

    continuar implementação inicial de WardUseCase

---
+++ src/Wards.Application/UsesCases/Wards/CriarWard/Commands/CriarWardCommand.cs
@@ -0,0 +1,22 @@
+﻿using Wards.Infrastructure.Data;
+
+namespace Wards.Application.UsesCases.Wards.CriarWard.Commands
+{
+    public sealed class CriarWardCommand
+    {
+        private readonly WardsContext _context;
+
+        public CriarWardCommand(WardsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Execute(Ward input)
+        {
+            await _context.AddAsync(input);
+            await _context.SaveChangesAsync();
+
+            return input.WardId;
+        }
+    }
+}
