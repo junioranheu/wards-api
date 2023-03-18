39a492bb39e7fff5997c7db43a16438c39496bbe
junioranheu <junioranheu@gmail.com>
2023-03-18 11:16:12 -0300

    iniciar implementação de UseCases/Ward

---
+++ src/Wards.Application/UsesCases/Wards/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Wards.Application.UsesCases.Wards
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWardsApplication(this IServiceCollection services)
+        {
+            services.AddScoped<ICriarWardUseCase, CriarWardUseCase>();
+            services.AddScoped<ICriarWardCommand, CriarWardCommand>();
+
+            services.AddScoped<IDeletarWardUseCase, DeletarWardUseCase>();
+            services.AddScoped<IDeletarWardCommand, DeletarWardCommand>();
+
+            services.AddScoped<IListarWardUseCase, ListarWardUseCase>();
+            services.AddScoped<IListarWardQuery, ListarWardQuery>();
+
+            services.AddScoped<IObterWardUseCase, ObterWardUseCase>();
+            services.AddScoped<IObterWardQuery, ObterWardQuery>();
+
+            return services;
+        }
+    }
+}
