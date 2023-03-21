a65821abc9d26923e88a2177c3224fc18e98bd78
junioranheu <junioranheu@gmail.com>
2023-03-20 21:21:06 -0300

    continuar implementação de Atualizar Ward

--- src/Wards.Application/UsesCases/Wards/AtualizarWard/Commands/AtualizarWardCommand.cs
+++ src/Wards.Application/UsesCases/Wards/AtualizarWard/Commands/AtualizarWardCommand.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Wards.Application.UsesCases.Wards.ObterWard.Queries;
+using Wards.Domain.Entities;
+using Wards.Infrastructure.Data;
+using static Wards.Utils.Common;
 
 namespace Wards.Application.UsesCases.Wards.AtualizarWard.Commands
 {
-    internal class AtualizarWardCommand
+    public sealed class AtualizarWardCommand : IAtualizarWardCommand
     {
+        private readonly WardsContext _context;
+        private readonly IObterWardQuery _obterQuery;
+
+        public AtualizarWardCommand(WardsContext context, IObterWardQuery obterQuery)
+        {
+            _context = context;
+            _obterQuery = obterQuery;
+        }
+
+        public async Task<int> Execute(Ward input)
+        {
+            var item = await _obterQuery.Execute(input.WardId);
+
+            if (item is null)
+                return 0;
+
+            item.Conteudo = !String.IsNullOrEmpty(input.Conteudo) ? input.Conteudo : item.Conteudo;
+            item.Data = HorarioBrasilia();
+            item.IsAtivo = input.IsAtivo;
+
+            _context.Update(item);
+            await _context.SaveChangesAsync();
+
+            return input.WardId;
+        }
     }
 }
---
+++ src/Wards.Application/UsesCases/Wards/AtualizarWard/Commands/IAtualizarWardCommand.cs
@@ -0,0 +1,9 @@
+﻿using Wards.Domain.Entities;
+
+namespace Wards.Application.UsesCases.Wards.AtualizarWard.Commands
+{
+    public interface IAtualizarWardCommand
+    {
+        Task<int> Execute(Ward input);
+    }
+}
