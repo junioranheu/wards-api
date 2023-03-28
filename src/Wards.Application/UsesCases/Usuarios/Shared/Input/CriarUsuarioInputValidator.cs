be7fa84cafb917252241c076e556b961c2cefad1
junioranheu <junioranheu@gmail.com>
2023-03-28 18:33:54 -0300

    novos ajustes em CriarUsuarioInputValidator

--- src/Wards.Application/UsesCases/Usuarios/Shared/Input/CriarUsuarioInputValidator.cs
+++ src/Wards.Application/UsesCases/Usuarios/Shared/Input/CriarUsuarioInputValidator.cs
@@ -9,14 +9,14 @@ namespace Wards.Application.UsesCases.Usuarios.Shared.Input
         {
             RuleFor(x => x.NomeCompleto).NotNull().NotEmpty().MinimumLength(3);
             RuleFor(x => x.NomeUsuarioSistema).NotNull().NotEmpty().MinimumLength(3);
-            RuleFor(x => x.Email).NotNull().NotEmpty().Must((x, email) => { return ValidarEmail(email: email); }).WithMessage("O e-mail é inválido");
+            RuleFor(x => x.Email).NotNull().NotEmpty().Must((rootObj, obj) => { return ValidarEmail(email: rootObj.Email); }).WithMessage("O e-mail é inválido");
 
-            RuleFor(x => x.Senha).NotNull().NotEmpty().Must((x, senha, context) =>
+            RuleFor(x => x.Senha).NotNull().NotEmpty().Must((rootObj, obj, context) =>
             {
-                var validarSenha = ValidarSenha(senha: x.Senha!, nomeCompleto: x.NomeCompleto!, nomeUsuario: x.NomeUsuarioSistema!, email: x.Email!);
-                context.MessageFormatter.AppendArgument("AvisoValidarSenha", validarSenha.Item2);
+                var validarSenha = ValidarSenha(senha: rootObj.Senha!, nomeCompleto: rootObj.NomeCompleto!, nomeUsuario: rootObj.NomeUsuarioSistema!, email: rootObj.Email!);
+                context.MessageFormatter.AppendArgument("_avisoValidarSenha", validarSenha.Item2);
                 return validarSenha.Item1;
-            }).WithMessage("{AvisoValidarSenha}");
+            }).WithMessage("{_avisoValidarSenha}");
 
             //RuleFor(x => x.Senha).NotNull().NotEmpty().WithMessage("Sua senha não pode ser vazia").
             //    MinimumLength(6).WithMessage("Sua senha não pode ter menos de 6 caracteres").
@@ -27,10 +27,10 @@ namespace Wards.Application.UsesCases.Usuarios.Shared.Input
             //    Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]").WithMessage("Sua senha deve ter pelo menos um caracter especial");
 
             RuleFor(x => x.Chamado).NotNull().NotEmpty();
-            RuleFor(x => x.UsuariosRolesId).Must((x, usuariosRolesId) =>
+            RuleFor(x => x.UsuariosRolesId).Must((rootObj, obj) =>
             {
-                if (usuariosRolesId is not null)
-                    if (usuariosRolesId.Contains(0))
+                if (rootObj.UsuariosRolesId is not null)
+                    if (rootObj.UsuariosRolesId.Contains(0))
                         return false;
 
                 return true;
