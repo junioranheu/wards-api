498f0da8d92cbce1024df81c45f72dd913b2e108
junioranheu <junioranheu@gmail.com>
2023-04-06 23:46:13 -0300

    melhorar TemperaturaWorker()

--- src/Wards.Workers/TemperaturaWorker/TemperaturaWorker.cs
+++ src/Wards.Workers/TemperaturaWorker/TemperaturaWorker.cs
@@ -11,18 +11,28 @@ namespace Wards.Workers.TemperaturaWorker
             StdSchedulerFactory factory = new();
             IScheduler scheduler = await factory.GetScheduler();
 
-            await scheduler.Start();
+            try
+            {
+                await scheduler.Start();
 
-            IJobDetail job = JobBuilder.Create<HelloJob>().
-                             Build();
+                IJobDetail job = JobBuilder.Create<HelloJob>().
+                                 Build();
 
-            ITrigger trigger = TriggerBuilder.Create().
-                               StartNow().
-                               WithSimpleSchedule(x => x.WithIntervalInSeconds(3).RepeatForever()).
-                               Build();
+                ITrigger trigger = TriggerBuilder.Create().
+                                   StartNow().
+                                   WithSimpleSchedule(x => x.WithIntervalInSeconds(3).RepeatForever()).
+                                   Build();
 
-            await scheduler.ScheduleJob(job, trigger);
-            // await scheduler.Shutdown();
+                //ITrigger trigger = TriggerBuilder.Create().
+                //                   WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(23, 45)).
+                //                   Build();
+
+                await scheduler.ScheduleJob(job, trigger);
+            }
+            catch (Exception)
+            {
+                await scheduler.Shutdown();
+            }
         }
     }
 }
