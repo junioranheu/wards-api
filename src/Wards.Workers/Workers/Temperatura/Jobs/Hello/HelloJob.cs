e45d8474990e96b52a63bfe8e6723ab334e5f64d
junioranheu <junioranheu@gmail.com>
2023-04-08 10:37:32 -0300

    teste DI em HelloJob

--- src/Wards.Workers/Workers/Temperatura/Jobs/Hello/HelloJob.cs
+++ src/Wards.Workers/Workers/Temperatura/Jobs/Hello/HelloJob.cs
@@ -1,10 +1,21 @@
-﻿using Quartz;
+﻿using AutoMapper;
+using Quartz;
+using Wards.Application.UsesCases.Usuarios.ListarUsuario.Queries;
 using static Wards.Utils.Common;
 
 namespace Wards.WorkersServices.Workers.Temperatura.Jobs.Hello
 {
     public sealed class HelloJob : IJob, IHelloJob
     {
+        private readonly IMapper _map;
+        private readonly IListarUsuarioQuery _listarQuery;
+
+        public HelloJob(IMapper map, IListarUsuarioQuery listarQuery)
+        {
+            _map = map;
+            _listarQuery = listarQuery;
+        }
+
         public async Task Execute(IJobExecutionContext context)
         {
             await Console.Out.WriteLineAsync($"Olá! {HorarioBrasilia()}");
