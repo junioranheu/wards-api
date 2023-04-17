857e4494aebc5a6eab58297daaac5e1cb3eedbfb
junioranheu <junioranheu@gmail.com>
2023-04-17 12:47:50 -0300

    refactor Logs

--- src/Wards.Application/UsesCases/Logs/CriarLog/Commands/CriarLogCommand.cs
+++ src/Wards.Application/UsesCases/Logs/CriarLog/Commands/CriarLogCommand.cs
@@ -1,23 +1,36 @@
-﻿using Wards.Domain.Entities;
+﻿using Microsoft.Extensions.Logging;
+using Wards.Application.UsesCases.Auxiliares.ListarEstado.Queries;
+using Wards.Domain.Entities;
 using Wards.Infrastructure.Data;
+using static Wards.Utils.Common;
 
 namespace Wards.Application.UsesCases.Logs.CriarLog.Commands
 {
     public sealed class CriarLogCommand : ICriarLogCommand
     {
         private readonly WardsContext _context;
+        private readonly ILogger _logger;
 
-        public CriarLogCommand(WardsContext context)
+        public CriarLogCommand(WardsContext context, ILogger<ListarEstadoQuery> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task Execute(Log input)
         {
-            _context.ChangeTracker.Clear();
+            try
+            {
+                _context.ChangeTracker.Clear();
 
-            await _context.AddAsync(input);
-            await _context.SaveChangesAsync();
+                await _context.AddAsync(input);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, HorarioBrasilia().ToString());
+                throw;
+            }
         }
     }
 }
--- src/Wards.Application/UsesCases/Logs/ListarLog/Queries/ListarLogQuery.cs
+++ src/Wards.Application/UsesCases/Logs/ListarLog/Queries/ListarLogQuery.cs
@@ -1,27 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Wards.Application.UsesCases.Auxiliares.ListarEstado.Queries;
 using Wards.Domain.Entities;
 using Wards.Infrastructure.Data;
+using static Wards.Utils.Common;
 
 namespace Wards.Application.UsesCases.Logs.ListarLog.Queries
 {
     public sealed class ListarLogQuery : IListarLogQuery
     {
         private readonly WardsContext _context;
+        private readonly ILogger _logger;
 
-        public ListarLogQuery(WardsContext context)
+        public ListarLogQuery(WardsContext context, ILogger<ListarEstadoQuery> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<IEnumerable<Log>> Execute(int pagina, int tamanhoPagina)
         {
-            var linq = await _context.Logs.
-                             Include(u => u.Usuarios).
-                             Skip(pagina * tamanhoPagina).Take(tamanhoPagina).
-                             OrderByDescending(l => l.LogId).
-                             AsNoTracking().ToListAsync();
+            try
+            {
+                var linq = await _context.Logs.
+                                 Include(u => u.Usuarios).
+                                 Skip(pagina * tamanhoPagina).Take(tamanhoPagina).
+                                 OrderByDescending(l => l.LogId).
+                                 AsNoTracking().ToListAsync();
 
-            return linq;
+                return linq;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, HorarioBrasilia().ToString());
+                throw;
+            }
         }
     }
 }
