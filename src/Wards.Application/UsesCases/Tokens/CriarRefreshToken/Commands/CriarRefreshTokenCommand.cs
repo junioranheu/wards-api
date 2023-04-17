85cb6132e5ac86df6e61c41f8becf5a1511a7e62
junioranheu <junioranheu@gmail.com>
2023-04-17 12:49:55 -0300

    refactor Tokens

--- src/Wards.Application/UsesCases/Tokens/CriarRefreshToken/Commands/CriarRefreshTokenCommand.cs
+++ src/Wards.Application/UsesCases/Tokens/CriarRefreshToken/Commands/CriarRefreshTokenCommand.cs
@@ -1,21 +1,34 @@
-﻿using Wards.Domain.Entities;
+﻿using Microsoft.Extensions.Logging;
+using Wards.Application.UsesCases.Auxiliares.ListarEstado.Queries;
+using Wards.Domain.Entities;
 using Wards.Infrastructure.Data;
+using static Wards.Utils.Common;
 
 namespace Wards.Application.UsesCases.Tokens.CriarRefreshToken.Commands
 {
     public sealed class CriarRefreshTokenCommand : ICriarRefreshTokenCommand
     {
         private readonly WardsContext _context;
+        private readonly ILogger _logger;
 
-        public CriarRefreshTokenCommand(WardsContext context)
+        public CriarRefreshTokenCommand(WardsContext context, ILogger<ListarEstadoQuery> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task Execute(RefreshToken input)
         {
-            await _context.AddAsync(input);
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.AddAsync(input);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, HorarioBrasilia().ToString());
+                throw;
+            }
         }
     }
 }
--- src/Wards.Application/UsesCases/Tokens/ObterRefreshToken/Queries/ObterRefreshTokenQuery.cs
+++ src/Wards.Application/UsesCases/Tokens/ObterRefreshToken/Queries/ObterRefreshTokenQuery.cs
@@ -1,27 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Wards.Application.UsesCases.Auxiliares.ListarEstado.Queries;
 using Wards.Infrastructure.Data;
+using static Wards.Utils.Common;
 
 namespace Wards.Application.UsesCases.Tokens.ObterRefreshToken.Queries
 {
     public sealed class ObterRefreshTokenQuery : IObterRefreshTokenQuery
     {
         private readonly WardsContext _context;
+        private readonly ILogger _logger;
 
-        public ObterRefreshTokenQuery(WardsContext context)
+        public ObterRefreshTokenQuery(WardsContext context, ILogger<ListarEstadoQuery> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<string> Execute(int id)
         {
-            // É necessário verificar se o refresh token com base no id do usuário e se o usuário de fato está ativo...
-            // Isso para ajudar numa possível "black-list";
-            var linq = await _context.RefreshTokens.
-                             Include(u => u.Usuarios).
-                             Where(r => r.UsuarioId == id && r.Usuarios!.IsAtivo == true).
-                             AsNoTracking().FirstOrDefaultAsync();
+            try
+            {
+                // É necessário verificar se o refresh token com base no id do usuário e se o usuário de fato está ativo...
+                // Isso para ajudar numa possível "black-list";
+                var linq = await _context.RefreshTokens.
+                                 Include(u => u.Usuarios).
+                                 Where(r => r.UsuarioId == id && r.Usuarios!.IsAtivo == true).
+                                 AsNoTracking().FirstOrDefaultAsync();
 
-            return linq?.RefToken ?? string.Empty;
+                return linq?.RefToken ?? string.Empty;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, HorarioBrasilia().ToString());
+                throw;
+            }
         }
     }
 }
