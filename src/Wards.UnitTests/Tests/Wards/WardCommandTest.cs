b7e361c57d4052bc611447c5044a554d9130bb02
junioranheu <junioranheu@gmail.com>
2023-04-18 16:57:33 -0300

    refactor WardCommandTest

--- src/Wards.UnitTests/Tests/Wards/WardCommandTest.cs
+++ src/Wards.UnitTests/Tests/Wards/WardCommandTest.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Wards.Application.UsesCases.Wards.CriarWard.Commands;
@@ -23,11 +24,12 @@ namespace Wards.UnitTests.Tests.Wards
         }
 
         [Theory]
-        [InlineData("a", "Para criar uma API você deverá bla bla bla", 1, false)]
-        [InlineData("Como criar uma API", "a", 2, false)]
+        [InlineData("a", "Para criar uma API você deverá bla bla bla", 1, true)]
+        [InlineData("Como criar uma API", "a", 2, true)]
+        [InlineData(null, "", 0, false)]
         [InlineData("", "", 0, false)]
-        [InlineData("Como criar uma API", "Para criar uma API você deverá bla bla bla", 0, false)]
-        [InlineData("Como criar uma API", "Para criar uma API você deverá bla bla bla", null, false)]
+        [InlineData("Como criar uma API", "Para criar uma API você deverá bla bla bla", 0, true)]
+        [InlineData("Como criar uma API", "Para criar uma API você deverá bla bla bla", null, true)]
         [InlineData("Como criar uma API", "Para criar uma API você deverá bla bla bla", 10, true)]
         public async Task CriarWardCommand_ChecarResultadoEsperado(string titulo, string conteudo, int? usuarioId, bool esperado)
         {
@@ -40,10 +42,10 @@ namespace Wards.UnitTests.Tests.Wards
 
             // Act;
             var resp = await command.Execute(_map.Map<Ward>(input));
-            //DBBBBBBBBB
+            var db = await _context.Wards.FirstOrDefaultAsync(x => x.Titulo == titulo);
 
             // Assert;
-            Assert.Equal(resp > 0, esperado);
+            Assert.Equal(db is not null, esperado);
         }
     }
 }
