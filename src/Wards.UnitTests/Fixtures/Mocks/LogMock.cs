ea0eb5644f20188fe008dcd1918cfff9374ad00c
junioranheu <junioranheu@gmail.com>
2023-06-30 17:37:21 -0300

    finalizar LogUseCaseTest

--- src/Wards.UnitTests/Fixtures/Mocks/LogMock.cs
+++ src/Wards.UnitTests/Fixtures/Mocks/LogMock.cs
@@ -1,10 +1,37 @@
 ﻿using Wards.Application.UseCases.Logs.Shared.Input;
+using Wards.Domain.Entities;
 using static Wards.Utils.Fixtures.Get;
 
 namespace Wards.UnitTests.Fixtures.Mocks
 {
     public static class LogMock
     {
+        public static Log CriarLog(string tipoRequisicao, string endpoint, string parametros, string descricao, int statusResposta, int usuarioId)
+        {
+            Log log = new()
+            {
+                TipoRequisicao = !string.IsNullOrEmpty(tipoRequisicao) ? tipoRequisicao : GerarStringAleatoria(5, false),
+                Endpoint = !string.IsNullOrEmpty(endpoint) ? endpoint : GerarStringAleatoria(5, false),
+                Parametros = !string.IsNullOrEmpty(parametros) ? parametros : GerarStringAleatoria(5, false),
+                Descricao = !string.IsNullOrEmpty(descricao) ? descricao : GerarStringAleatoria(5, false),
+                StatusResposta = statusResposta > 0 ? statusResposta : GerarNumeroAleatorio(1, 999),
+                UsuarioId = usuarioId > 0 ? usuarioId : GerarNumeroAleatorio(1, 999)
+            };
+
+            return log;
+        }
+
+        public static IEnumerable<Log> CriarListaLog()
+        {
+            List<Log> lista = new()
+            {
+                CriarLog(string.Empty, string.Empty, string.Empty, string.Empty, 0, 0),
+                CriarLog(string.Empty, string.Empty, string.Empty, string.Empty, 0, 0)
+            };
+
+            return lista;
+        }
+
         public static LogInput CriarLogInput(string tipoRequisicao, string endpoint, string parametros, string descricao, int statusResposta, int usuarioId)
         {
             LogInput log = new()
--- src/Wards.UnitTests/Tests/Logs/LogUseCaseTest.cs
+++ src/Wards.UnitTests/Tests/Logs/LogUseCaseTest.cs
@@ -2,7 +2,10 @@
 using Moq;
 using Wards.Application.UseCases.Logs.CriarLog;
 using Wards.Application.UseCases.Logs.CriarLog.Commands;
+using Wards.Application.UseCases.Logs.ListarLog;
+using Wards.Application.UseCases.Logs.ListarLog.Queries;
 using Wards.Application.UseCases.Logs.Shared.Input;
+using Wards.Application.UseCases.Shared.Models.Input;
 using Wards.Domain.Entities;
 using Wards.UnitTests.Fixtures;
 using Wards.UnitTests.Fixtures.Mocks;
@@ -20,7 +23,8 @@ namespace Wards.UnitTests.Tests.Logs
         }
 
         [Theory]
-        [InlineData("xxx", "xxx", "xxx", "xxx", 0, 0, true)]
+        [InlineData("POST", "TesteController/Teste", "Teste", "Isso é um teste válido", 200, 1, true)]
+        [InlineData("", "", "", "Isso é um teste inválido", 0, 0, false)]
         public async Task Criar_ChecarResultadoEsperado(string tipoRequisicao, string endpoint, string parametros, string descricao, int statusResposta, int usuarioId, bool esperado)
         {
             // Arrange;
@@ -31,11 +35,36 @@ namespace Wards.UnitTests.Tests.Logs
 
             LogInput input = LogMock.CriarLogInput(tipoRequisicao, endpoint, parametros, descricao, statusResposta, usuarioId);
 
+            try
+            {
+                // Act;
+                await useCase.Execute(input);
+
+                // Assert;
+                Assert.True(esperado);
+            }
+            catch (Exception)
+            {
+                // Assert;
+                Assert.False(esperado);
+            }
+        }
+
+        [Fact]
+        public async Task Listar_ChecarResultadoEsperado()
+        {
+            // Arrange;
+            var paginacao = new Mock<PaginacaoInput>();
+            var listarQuery = new Mock<IListarLogQuery>();
+            listarQuery.Setup(x => x.Execute(It.IsAny<PaginacaoInput>())).Returns(Task.FromResult(LogMock.CriarListaLog()));
+
+            var useCase = new ListarLogUseCase(_map, listarQuery.Object);
+
             // Act;
-            await useCase.Execute(input);
+            var teste = await useCase.Execute(paginacao.Object);
 
             // Assert;
-            //Assert.Equal(esperado);
+            Assert.True(teste.Count() > 0);
         }
     }
 }
