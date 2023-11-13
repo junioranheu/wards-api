b18978c2b627bac5e7cdc223810aa4dbae1627f5
junioranheu <junioranheu@gmail.com>
2023-11-12 21:58:39 -0300

    implementar ObterStatusBanco

--- src/Wards.API/Controllers/AuxiliaresController.cs
+++ src/Wards.API/Controllers/AuxiliaresController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DocumentFormat.OpenXml.Vml;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Wards.Application.UseCases.Auxiliares.ListarEstado;
 using Wards.Application.UseCases.Auxiliares.ListarEstado.Shared.Output;
 using Wards.Application.UseCases.Shared.Models.Input;
 using Wards.Domain.Consts;
 using Wards.Domain.Enums;
+using Wards.Infrastructure.Factory.ConnectionFactory;
 using static Wards.Utils.Fixtures.Get;
 
 namespace Wards.API.Controllers
@@ -13,11 +16,38 @@ namespace Wards.API.Controllers
     [ApiController]
     public class AuxiliaresController : Controller
     {
+        private readonly IConnectionFactory _connectionFactory;
         private readonly IListarEstadoUseCase _listarEstadoUseCase;
 
-        public AuxiliaresController(IListarEstadoUseCase listarEstadoUseCase)
+        public AuxiliaresController(
+            IConnectionFactory connectionFactory,
+            IListarEstadoUseCase listarEstadoUseCase)
         {
+            _connectionFactory = connectionFactory;
             _listarEstadoUseCase = listarEstadoUseCase;
+        }
+
+        [HttpGet("obterStatusBanco")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof((bool, string)))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusCodes))]
+        public async Task<ActionResult<(bool, string)>> ObterStatusBanco(CancellationToken cancellationToken = default)
+        {
+            bool isOk = true;
+            string desc = string.Empty;
+
+            try
+            {
+                var connection = _connectionFactory.ObterMySqlConnection();
+
+                await connection.OpenAsync(cancellationToken);
+                return Ok(new { isOk, desc });
+            }
+            catch (Exception ex)
+            {
+                isOk = false;
+                return Ok(new { isOk, ex.Message});
+            }
         }
 
         [HttpGet("listarEstado")]
