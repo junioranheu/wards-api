3b185d9f30105b7f41de79bee2c41afe309bd658
Junior <junioranheu@gmail.com>
2024-09-04 11:49:48 -0300

    refactor IGenericCacheService.cs

--- src/Wards.Application/Services/Cache/GenericCache/IGenericCacheService.cs
+++ src/Wards.Application/Services/Cache/GenericCache/IGenericCacheService.cs
@@ -1,7 +1,11 @@
-﻿namespace Wards.Application.Services.Cache.GenericCache;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wards.Application.Services.Cache.GenericCache;
 
 public interface IGenericCacheService
 {
-    void Clear(string[] keys);
     Task<T?> GetOrAdd<T>(string key, Func<Task<T>> fetchFunction, TimeSpan expiration);
+    Task<T?> Get<T>(string key);
+    Task<T?> GetObjectFromActionResult<T>(Func<Task<ActionResult>> fetchFunction, string key) where T : class;
+    void Clear(string[] keys);
 }
